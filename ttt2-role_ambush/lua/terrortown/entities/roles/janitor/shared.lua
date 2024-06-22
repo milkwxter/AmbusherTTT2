4c9ed3a38a2fd9f5345b7d2d015c25ac3019db30
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-22 13:29:27 -0500

    initial role commit

---
+++ ttt2-role_ambush/lua/terrortown/entities/roles/janitor/shared.lua
@@ -0,0 +1,37 @@
+if SERVER then
+  AddCSLuaFile()
+  resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_ambush.vmt")
+end
+
+function ROLE:PreInitialize()
+  self.color = Color(174, 56, 1, 255)
+
+  self.abbr = "ambush" -- abbreviation
+  self.surviveBonus = 0.5 -- bonus multiplier for every survive while another player was killed
+  self.scoreKillsMultiplier = 5 -- multiplier for kill of player of another team
+  self.scoreTeamKillsMultiplier = -16 -- multiplier for teamkill
+  self.preventFindCredits = false
+  self.preventKillCredits = false
+  self.preventTraitorAloneCredits = false
+  
+  self.isOmniscientRole = true
+
+  self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
+  self.defaultTeam = TEAM_TRAITOR
+
+  self.conVarData = {
+    pct = 0.17, -- necessary: percentage of getting this role selected (per player)
+    maximum = 1, -- maximum amount of roles in a round
+    minPlayers = 6, -- minimum amount of players until this role is able to get selected
+    credits = 1, -- the starting credits of a specific role
+    togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+    random = 33,
+    traitorButton = 1, -- can use traitor buttons
+    shopFallback = SHOP_FALLBACK_TRAITOR
+  }
+end
+
+-- now link this subrole with its baserole
+function ROLE:Initialize()
+  roles.SetBaseRole(self, ROLE_TRAITOR)
+end
---
+++ ttt2-role_ambush/lua/terrortown/lang/en/ambusher.lua
@@ -0,0 +1,10 @@
+local L = LANG.GetLanguageTableReference("en")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[AMBUSHER.name] = "Ambusher"
+L["info_popup_" .. AMBUSHER.name] = [[You are the Ambusher! Stand still to sense nearby players and gain a damage boost.]]
+L["body_found_" .. AMBUSHER.abbr] = "They were a Ambusher."
+L["search_role_" .. AMBUSHER.abbr] = "This person was a Ambusher!"
+L["target_" .. AMBUSHER.name] = "Ambusher"
+L["ttt2_desc_" .. AMBUSHER.name] = [[The Ambusher can stand still to see his prey.]]
+L["credit_" .. AMBUSHER.abbr .. "_all"] = "Ambusher, you have been awarded {num} equipment credit(s) for your performance."
